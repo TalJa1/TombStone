daf98b46b7cdabbb31a74ba6947ac41c1f685825
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-21 14:10:30 +0700

    Refactor typeProps.ts to add MartyrProfileItem interface

--- services/typeProps.ts
+++ services/typeProps.ts
@@ -3,3 +3,25 @@ export interface OnboardingItem {
   description: string;
   img: any; // You can replace 'any' with the specific type if you know it
 }
+
+export interface MartyrProfileItem {
+  giayBaoTu: any;
+  banTrichLuc: any;
+  giayNoiHisinh: any;
+  giayKhac: any;
+  giayToCoChuVietLietSi: string[];
+  name: string;
+  birthYear: string;
+  sex: string;
+  hometown: string;
+  armyJoinDate: string;
+  level: string;
+  position: string;
+  unit: string;
+  deathDate: string;
+  deathPlace: string;
+  deathReason: string;
+  otherName: string;
+  relationship: string;
+  otherInfor: string;
+}
