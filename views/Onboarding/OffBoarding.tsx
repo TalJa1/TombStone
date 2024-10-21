adf04cc084005bb59f4b6636ef0a9cc098171987
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-21 12:04:10 +0700

    Refactor OffBoarding component to display title, image, and description

--- views/Onboarding/OffBoarding.tsx
+++ views/Onboarding/OffBoarding.tsx
@@ -1,14 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
+import {CheckStartPage} from '../../services/renderData';
+import { vw } from '../../services/styleSheet';
 
 const OffBoarding = () => {
   useStatusBar('#547958');
+  const renderData = CheckStartPage;
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>OffBoarding</Text>
+        <Text style={styles.title}>{renderData.title}</Text>
+        <Image source={renderData.img} style={styles.image} />
+        <Text style={styles.des}>{renderData.description}</Text>
       </View>
     </SafeAreaView>
   );
@@ -20,5 +26,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#547958',
+  },
+  image: {
+    width: vw(80),
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '400',
+    color: '#F9D000',
+    marginTop: vw(5),
+  },
+  des: {
+    fontSize: 14,
+    color: '#ECF3A3',
+    textAlign: 'center',
+    paddingHorizontal: vw(5),
+    marginTop: vw(2),
   },
 });
