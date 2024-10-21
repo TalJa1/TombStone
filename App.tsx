5deadbe9e85a333302c24deea50dc56b30a34428
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-21 09:54:50 +0700

    Refactor bottom tab navigation and add custom icons

--- App.tsx
+++ App.tsx
@@ -1,118 +1,167 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {StyleSheet, Text, View} from 'react-native';
+import {vh, vw} from './services/styleSheet';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  communityFilledIcon,
+  communityIcon,
+  homeFilledIcon,
+  homeIcon,
+  mapFilledIcon,
+  mapIcon,
+  searchFilledIcon,
+  searchIcon,
+} from './assets/svgXML';
+import Home from './views/bottomTabs/Home';
+import SearchPage from './views/bottomTabs/SearchPage';
+import MapPage from './views/bottomTabs/MapPage';
+import CommunityPage from './views/bottomTabs/CommunityPage';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+const App = () => {
+  const TabNavigator = () => {
+    return (
+      <View style={styles.tabnavigationStyle}>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: '#547958',
+            tabBarShowLabel: false,
+            tabBarStyle: {
+              borderTopColor: '#FFFFFF',
+              backgroundColor: '#FFFFFF',
+              height: vh(10),
+            },
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+          <Tab.Screen
+            name="Trang chủ"
+            component={Home}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(8) : vw(8);
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+                return (
+                  <View style={[styles.iconContainer]}>
+                    {focused
+                      ? homeFilledIcon(iconSize, iconSize, color)
+                      : homeIcon(iconSize, iconSize, color)}
+                    {focused ? (
+                      <Text style={[{color: color}, styles.label]}>
+                        Trang chủ
+                      </Text>
+                    ) : null}
+                  </View>
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Tìm kiếm"
+            component={SearchPage}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(8) : vw(8);
+
+                return (
+                  <View style={[styles.iconContainer]}>
+                    {focused
+                      ? searchFilledIcon(iconSize, iconSize, color)
+                      : searchIcon(iconSize, iconSize, color)}
+                    {focused ? (
+                      <Text style={[{color: color}, styles.label]}>
+                        Tìm kiếm
+                      </Text>
+                    ) : null}
+                  </View>
+                );
+              },
+            }}
+          />
+
+          <Tab.Screen
+            name="Bản đồ"
+            component={MapPage}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({focused, color}) => {
+                const iconSize = focused ? vw(8) : vw(8);
+
+                return (
+                  <View style={[styles.iconContainer]}>
+                    {focused
+                      ? mapFilledIcon(iconSize, iconSize, color)
+                      : mapIcon(iconSize, iconSize, color)}
+                    {focused ? (
+                      <Text style={[{color: color}, styles.label]}>Bản đồ</Text>
+                    ) : null}
+                  </View>
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Cộng đồng"
+            component={CommunityPage}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(8) : vw(8);
+
+                return (
+                  <View style={[styles.iconContainer]}>
+                    {focused
+                      ? communityFilledIcon(iconSize, iconSize, color)
+                      : communityIcon(iconSize, iconSize, color)}
+                    {focused ? (
+                      <Text style={[{color: color}, styles.label]}>
+                        Cộng đồng
+                      </Text>
+                    ) : null}
+                  </View>
+                );
+              },
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+    );
+  };
+  return (
+    <NavigationContainer>
+      {/* Main || Onboarding */}
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Main"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  tabnavigationStyle: {backgroundColor: '#1A1A1A', flex: 1},
+  iconContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
+    top: vh(1),
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: '700',
+    position :'relative',
+    top: vh(0.5),
+  },
+});
