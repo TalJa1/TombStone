99c6208e3c1ac1232631366f586b235ce0b7cff0
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-21 10:41:09 +0700

    Refactor onboarding data and add new screens with descriptions and images

--- services/renderData.ts
+++ services/renderData.ts
@@ -1,1 +1,38 @@
-export const OnboardingData = [{}];
+export const OnboardingData = [
+  {
+    title: 'Kết nối yêu thương',
+    description:
+      'Cùng bạn tìm kiếm và xác định mộ của người thân thất lạc, mang lại cơ hội tìm lại ký ức và tình yêu đã mất',
+    img: require('../assets/Onboarding/o1.png'),
+  },
+  {
+    title: 'Tìm Kiếm Thông Tin Mộ',
+    description:
+      'Nhập thông tin về người thân hoặc mộ cần tìm,  hệ thống sẽ giúp bạn tra cứu nhanh chóng các kết quả liên quan bằng việc số hóa bia mộ liệt sĩ.',
+    img: require('../assets/Onboarding/o2.png'),
+  },
+  {
+    title: 'Giúp Đỡ Cộng Đồng',
+    description:
+      'Nếu bạn tìm thấy một ngôi mộ không rõ danh tính, hãy báo cáo để giúp gia đình người khác tìm lại người thân của họ',
+    img: require('../assets/Onboarding/o3.png'),
+  },
+  {
+    title: 'Công Nghệ Xác Định ADN Hiện Đại',
+    description:
+      'Hợp tác với các cơ quan để giúp xác định ADN, hỗ trợ việc tìm kiếm và xác minh danh tính một cách chính xác',
+    img: require('../assets/Onboarding/o4.png'),
+  },
+  {
+    title: 'Không gian nhân văn',
+    description:
+      'Tạo không gian để các gia đình có thể hỗ trợ và chia sẻ kinh nghiệm trong việc tìm kiếm và tưởng niệm người thân',
+    img: require('../assets/Onboarding/o5.png'),
+  },
+  {
+    title: 'Hành trình tìm lại yêu thương bắt đầu',
+    description:
+      'Hãy bắt đầu hành trình của bạn để tìm lại người thân và hỗ trợ cộng đồng cùng “Dấu chân Ký Ức"',
+    img: require('../assets/Onboarding/o6.png'),
+  },
+];
