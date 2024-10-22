8df03bdb385042808e102d8d343377f6f9b2d507
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-22 09:47:30 +0700

    Refactor TombList.tsx to add header with back button and title

--- views/Searching/TombList.tsx
+++ views/Searching/TombList.tsx
@@ -1,18 +1,44 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
+import {backIcon} from '../../assets/svgXML';
+import {vw} from '../../services/styleSheet';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
 const TombList = () => {
   useStatusBar('white');
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1, paddingHorizontal: vw(5)}}>
+          <Header />
           <Text> TombList </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity
+        style={styles.headerBtn}
+        onPress={() => navigation.goBack()}>
+        {backIcon(vw(7), vw(7))}
+      </TouchableOpacity>
+      <Text style={styles.headerTitle}>Danh sách nghĩa trang</Text>
+    </View>
   );
 };
 
@@ -22,5 +48,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: vw(5),
+    columnGap: vw(5),
+  },
+  headerBtn: {
+    backgroundColor: '#91A89526',
+    borderRadius: vw(50),
+    padding: vw(2),
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#547958',
   },
 });
