6e3f2965280be42b4cf815e61ecf0f4adadc2e24
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-22 11:33:46 +0700

    Add HeaderComponent to AddPage screen

--- views/bottomTabs/AddPage.tsx
+++ views/bottomTabs/AddPage.tsx
@@ -2,12 +2,14 @@ import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
+import HeaderComponent from '../../components/HeaderComponent';
 
 const AddPage = () => {
   useStatusBar('white');
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <HeaderComponent title="Thêm hồ sơ liệt sĩ" />
         <View>
           <Text>AddPage</Text>
         </View>
