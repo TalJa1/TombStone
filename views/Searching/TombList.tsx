92ac001d4855b796ddd34372ba0e354498382bba
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-22 10:20:01 +0700

    Refactor TombList.tsx to add search functionality

--- views/Searching/TombList.tsx
+++ views/Searching/TombList.tsx
@@ -3,14 +3,15 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
-import {backIcon} from '../../assets/svgXML';
-import {vw} from '../../services/styleSheet';
+import {backIcon, searchIcon} from '../../assets/svgXML';
+import {vh, vw} from '../../services/styleSheet';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
 
@@ -21,10 +22,27 @@ const TombList = () => {
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={{flex: 1, paddingHorizontal: vw(5)}}>
           <Header />
+          <SearchView />
           <Text> TombList </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const SearchView: React.FC = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        {searchIcon(vw(5), vw(5), '#547958')}
+        <TextInput
+          inputMode="text"
+          style={styles.searchInput}
+          placeholder="Nhập tên nghĩa trang"
+          placeholderTextColor="#6FA078"
+        />
+      </View>
+    </View>
   );
 };
 
@@ -63,6 +81,24 @@ const styles = StyleSheet.create({
   headerTitle: {
     fontSize: 18,
     fontWeight: '700',
+    color: '#547958',
+  },
+  searchContainer: {
+    marginTop: vh(2),
+    marginHorizontal: vw(5),
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#547958',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    paddingLeft: 10,
     color: '#547958',
   },
 });
