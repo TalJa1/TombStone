fe44a92b9d9989a2ff7aee9090cd59dd45241850
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-23 15:38:21 +0700

    Refactor InforChecking component to include header with title and cancel button

--- views/AddPage/InforChecking.tsx
+++ views/AddPage/InforChecking.tsx
@@ -1,9 +1,19 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {InforDetail} from '../../services/typeProps';
+import {cancelIcon} from '../../assets/svgXML';
+import {vh, vw} from '../../services/styleSheet';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const InforChecking = () => {
   useStatusBar('white');
@@ -15,12 +25,32 @@ const InforChecking = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1}}>
+          <Header />
           <Text>InforChecking</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerTop}>
+        <Text style={styles.headerTitle}>Xác nhận thông tin</Text>
+        <TouchableOpacity
+          style={styles.headerBtn}
+          onPress={() => navigation.goBack()}>
+          {cancelIcon(vw(7), vw(7), '#91A895')}
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.headerDes}>
+        Hãy xác nhận thông tin hồ sơ lần cuối cùng.
+      </Text>
+    </View>
   );
 };
 
@@ -30,5 +60,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  header: {
+    backgroundColor: 'white',
+    padding: vw(5),
+    rowGap: vh(1.5),
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#000000',
+  },
+  headerDes: {
+    fontSize: 12,
+    color: '#000000',
+  },
+  headerBtn: {
+    alignSelf: 'flex-end',
+  },
+  headerTop: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
