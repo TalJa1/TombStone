386d428a0082dbebfbbe07ff98d5405ac5d012ea
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-23 15:12:31 +0700

    Refactor Page6 component to include personal information fields and update styling

--- components/Home/Page6.tsx
+++ components/Home/Page6.tsx
@@ -1,13 +1,23 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import { PageProps } from '../../services/typeProps';
-import { vw } from '../../services/styleSheet';
+import {PageProps} from '../../services/typeProps';
+import {vw} from '../../services/styleSheet';
+import TextInputComponent from '../AddPage/TextInputComponent';
 
-const Page6: React.FC<PageProps> = () => {
+const Page6: React.FC<PageProps> = ({data, setData}) => {
+  const handleDescriptionChange = (value: string) => {
+    setData({...data, description: value});
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Thông tin cá nhân của liệt sĩ</Text>
-      <Text>Page6</Text>
+      <Text style={styles.title}>Thông tin hỗ trợ/yêu cầu khác</Text>
+      <TextInputComponent
+        label="Quan hệ người tìm kiếm (bạn) với liệt sĩ"
+        value={data.description}
+        setValue={handleDescriptionChange}
+        type="text"
+        width={100}
+      />
     </View>
   );
 };
