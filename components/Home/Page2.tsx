665e401ace007aa9a4536020f8d33062880bba2c
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-23 11:44:42 +0700

    Refactor Page2.tsx to update personal information fields

--- components/Home/Page2.tsx
+++ components/Home/Page2.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {PageProps} from '../../services/typeProps';
@@ -9,6 +10,21 @@ const Page2: React.FC<PageProps> = ({setData, data}) => {
     setData({...data, name: value});
   };
 
+  const handleBirthYearChange = (value: string) => {
+    setData({...data, birthYear: value});
+  };
+
+  const handleSexChange = (value: string) => {
+    setData({...data, sex: value});
+  };
+
+  const handleHometownChange = (value: string) => {
+    setData({...data, hometown: value});
+  };
+
+  const handleArmyJoinDateChange = (value: string) => {
+    setData({...data, armyJoinDate: value});
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Thông tin cá nhân của liệt sĩ</Text>
@@ -17,6 +33,51 @@ const Page2: React.FC<PageProps> = ({setData, data}) => {
         value={data.name}
         setValue={handleNameChange}
         type="text"
+        width={100}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}>
+        <TextInputComponent
+          label="Năm sinh"
+          value={data.birthYear}
+          setValue={handleBirthYearChange}
+          type="number"
+          width={45}
+        />
+        <TextInputComponent
+          label="Giới tính"
+          value={data.sex}
+          setValue={handleSexChange}
+          type="select"
+          width={45}
+          options={[
+            {
+              label: 'Nam',
+              value: 'Nam',
+            },
+            {
+              label: 'Nữ',
+              value: 'Nữ',
+            },
+          ]}
+        />
+      </View>
+      <TextInputComponent
+        label="Quê quán"
+        value={data.hometown}
+        setValue={handleHometownChange}
+        type="text"
+        width={100}
+      />
+      <TextInputComponent
+        label="Ngày nhập ngũ"
+        value={data.armyJoinDate}
+        setValue={handleArmyJoinDateChange}
+        type="date"
         width={100}
       />
     </View>
