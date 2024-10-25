2055c0b2a8d235d3f872c0b9feeabcc5adba8d2d
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-25 11:32:55 +0700

    Refactor MapPage to use Mapbox dependency, update styling, and add GeoJSON source for provinces

--- views/bottomTabs/MapPage.tsx
+++ views/bottomTabs/MapPage.tsx
@@ -10,6 +10,53 @@ Mapbox.setAccessToken(
   'pk.eyJ1IjoidGFsamExIiwiYSI6ImNtMm82enp6ODBlNHAyanNndnNmMHowYWYifQ.-6cYPNPzMoKrlIp428Yxdw',
 );
 
+import {FeatureCollection, Geometry, GeoJsonProperties} from 'geojson';
+
+const geoJsonData: FeatureCollection<Geometry, GeoJsonProperties> = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Hà Nội',
+        density: 2390,
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [105.7974, 21.0285],
+            [105.7974, 20.9275],
+            [105.9157, 20.9275],
+            [105.9157, 21.0285],
+            [105.7974, 21.0285],
+          ],
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Hồ Chí Minh',
+        density: 4200,
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [106.6297, 10.8231],
+            [106.6297, 10.7231],
+            [106.7497, 10.7231],
+            [106.7497, 10.8231],
+            [106.6297, 10.8231],
+          ],
+        ],
+      },
+    },
+    // Add more provinces here
+  ],
+};
+
 const MapPage = () => {
   useStatusBar('#91A895');
   const mapRef = useRef<Mapbox.MapView>(null);
@@ -19,12 +66,38 @@ const MapPage = () => {
       <View style={{flex: 1}}>
         <View style={styles.page}>
           <View style={styles.containerMap}>
-            <Mapbox.MapView ref={mapRef} style={styles.map}>
+            <Mapbox.MapView
+              styleURL="mapbox://styles/mapbox/standard-satellite"
+              ref={mapRef}
+              style={styles.map}>
               <Mapbox.Camera
                 centerCoordinate={[105.8342, 21.0278]} // Hanoi, Vietnam
                 zoomLevel={5.5}
                 animationDuration={2000}
               />
+
+              {/* GeoJSON Source for provinces */}
+              <Mapbox.ShapeSource id="provinces" shape={geoJsonData}>
+                <Mapbox.FillLayer
+                  id="province-layer"
+                  style={{
+                    fillColor: [
+                      'interpolate',
+                      ['linear'],
+                      ['get', 'density'],
+                      0,
+                      'white',
+                      1000,
+                      '#ffbaba',
+                      3000,
+                      'red',
+                      5000,
+                      'black',
+                    ],
+                    fillOpacity: 0.7,
+                  }}
+                />
+              </Mapbox.ShapeSource>
             </Mapbox.MapView>
           </View>
         </View>
