bf5b157937c0a170db12febdc60045c7550049bc
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-28 17:04:41 +0700

    Refactor MartyrProfile component to add header UI with back button

--- views/Searching/MartyrProfile.tsx
+++ views/Searching/MartyrProfile.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {backIcon} from '../../assets/svgXML';
+import {vw} from '../../services/styleSheet';
 
 const MartyrProfile = () => {
   useStatusBar('white');
@@ -19,7 +29,18 @@ const MartyrProfile = () => {
 };
 
 const Header: React.FC = () => {
-  return <View></View>;
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity
+        style={styles.headerBtn}
+        onPress={() => navigation.goBack()}>
+        {backIcon(vw(6), vw(6), '#547958')}
+      </TouchableOpacity>
+      <Text style={styles.headerText}>Tìm kiếm hồ sơ liệt sĩ</Text>
+    </View>
+  );
 };
 
 export default MartyrProfile;
@@ -28,5 +49,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: vw(4),
+    paddingHorizontal: vw(5),
+    backgroundColor: 'white',
+    columnGap: vw(3),
+  },
+  headerBtn: {
+    alignItems: 'center',
+    backgroundColor: '#91A89526',
+    padding: vw(2),
+    borderRadius: vw(50),
+  },
+  headerText: {
+    fontSize: vw(4),
+    fontWeight: 'bold',
+    color: '#547958',
   },
 });
