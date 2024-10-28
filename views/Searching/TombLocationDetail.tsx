4d7abd257c7337b493999665ea742bdfbd81bd27
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-28 16:19:09 +0700

    Refactor TombLocationDetail component to optimize rendering logic, update filter functionality, and improve header UI

--- views/Searching/TombLocationDetail.tsx
+++ views/Searching/TombLocationDetail.tsx
@@ -38,8 +38,6 @@ const TombLocationDetail = () => {
   });
   const [renderFilter, setRenderFilter] = useState<any[]>([]);
 
-  console.log('renderfilter', renderFilter);
-
   const handleTabPress = (tab: string) => {
     setSelectedTab(tab);
   };
@@ -97,9 +95,23 @@ const TombLocationDetail = () => {
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={{flex: 1}}>
-          <Header title={headerTitle} setIsModalVisible={setIsModalVisible} />
-          <TabRender selectedTab={selectedTab} onTabPress={handleTabPress} />
-          {renderContent()}
+          <Header
+            title={headerTitle}
+            setIsModalVisible={setIsModalVisible}
+            renderingDes={renderFilter.length > 0 ? false : true}
+            filterData={filterData}
+          />
+          {renderFilter.length > 0 ? (
+            <View></View>
+          ) : (
+            <>
+              <TabRender
+                selectedTab={selectedTab}
+                onTabPress={handleTabPress}
+              />
+              {renderContent()}
+            </>
+          )}
         </View>
         <View style={{height: vh(5)}} />
       </ScrollView>
@@ -143,12 +155,67 @@ const TabRender: React.FC<{
 const Header: React.FC<{
   title: string;
   setIsModalVisible: (visible: boolean) => void;
-}> = ({title, setIsModalVisible}) => {
+  renderingDes: boolean;
+  filterData: {[key: string]: any};
+}> = ({title, setIsModalVisible, renderingDes, filterData}) => {
   const [isTextVisible, setIsTextVisible] = useState(true);
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
   const handleHidePress = () => {
     setIsTextVisible(false);
+  };
+
+  const renderFilterValues = () => {
+    return Object.keys(filterData)
+      .filter(key => filterData[key] !== null)
+      .map(key => {
+        const value = filterData[key];
+        let style;
+
+        if (value === 'Chưa xác định') {
+          style = {
+            backgroundColor: '#F7DAD4',
+            color: '#571B0F',
+          };
+        } else {
+          const randomStyle = Math.floor(Math.random() * 3);
+          switch (randomStyle) {
+            case 0:
+              style = {
+                color: 'white',
+                borderColor: 'white',
+                borderWidth: 1,
+              };
+              break;
+            case 1:
+              style = {
+                backgroundColor: '#ECF3A3',
+                color: '#547958',
+              };
+              break;
+            case 2:
+              style = {
+                backgroundColor: '#F7DAD4',
+                color: '#571B0F',
+              };
+              break;
+          }
+        }
+
+        return (
+          <View key={key}>
+            {value !== '' && (
+              <Text
+                style={[
+                  {fontSize: 12, padding: 5, borderRadius: 5, margin: 2},
+                  style,
+                ]}>
+                {value}
+              </Text>
+            )}
+          </View>
+        );
+      });
   };
 
   return (
@@ -167,51 +234,90 @@ const Header: React.FC<{
           <Text style={styles.headerTitle}>NTLS {title}</Text>
         </View>
       </View>
-      <View style={{width: vw(100)}}>
-        {isTextVisible ? (
-          <View style={styles.headerDes}>
-            <Text style={styles.headerDesTxt}>
-              Nghĩa trang liệt sĩ Quốc gia {title} là nơi quy tụ 10.263 phần mộ
-              của các liệt sĩ có tổng diện tích 140.000m2. Phần đất mộ được phân
-              thành 10 khu vực chính. Nghĩa trang liệt sĩ Trường Sơn là nơi an
-              nghỉ đời đời của các anh hùng liệt sĩ đã hi sinh trên tuyến đường
-              mòn Hồ Chí Minh trong thời kì chống Mỹ cứu nước.{' '}
-              <Text style={styles.hideButton} onPress={handleHidePress}>
-                Ẩn
+      {renderingDes ? (
+        <View style={{width: vw(100)}}>
+          {isTextVisible ? (
+            <View style={styles.headerDes}>
+              <Text style={styles.headerDesTxt}>
+                Nghĩa trang liệt sĩ Quốc gia {title} là nơi quy tụ 10.263 phần
+                mộ của các liệt sĩ có tổng diện tích 140.000m2. Phần đất mộ được
+                phân thành 10 khu vực chính. Nghĩa trang liệt sĩ Trường Sơn là
+                nơi an nghỉ đời đời của các anh hùng liệt sĩ đã hi sinh trên
+                tuyến đường mòn Hồ Chí Minh trong thời kì chống Mỹ cứu nước.{' '}
+                <Text style={styles.hideButton} onPress={handleHidePress}>
+                  Ẩn
+                </Text>
               </Text>
-            </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  columnGap: vw(4),
+                  marginTop: vh(1.5),
+                }}>
+                <TouchableOpacity
+                  style={styles.searchContainer}
+                  onPress={() => setIsModalVisible(true)}>
+                  <Text style={{color: '#D4D4D4', fontSize: 16}}>
+                    Tìm kiếm mộ liệt sĩ
+                  </Text>
+                  {filterIcon(vw(5), vw(5), 'white')}
+                </TouchableOpacity>
+                {searchIcon(vw(6), vw(6), 'white')}
+              </View>
+            </View>
+          ) : (
+            <View style={styles.searchIconContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  setIsTextVisible(true);
+                }}>
+                <View style={styles.searchCenterBtn}>
+                  {searchIcon(vw(7), vw(7), 'white')}
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      ) : (
+        <View style={styles.headerDes}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              columnGap: vw(4),
+              marginTop: vh(1.5),
+            }}>
+            <TouchableOpacity
+              style={[styles.searchContainer, {borderRadius: 8}]}
+              onPress={() => setIsModalVisible(true)}>
+              <Text style={{color: '#D4D4D4', fontSize: 16}}>
+                Tìm kiếm mộ liệt sĩ
+              </Text>
+              {filterIcon(vw(5), vw(5), 'white')}
+            </TouchableOpacity>
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                columnGap: vw(4),
-                marginTop: vh(1.5),
+                backgroundColor: 'white',
+                padding: vw(2),
+                borderRadius: 16,
               }}>
-              <TouchableOpacity
-                style={styles.searchContainer}
-                onPress={() => setIsModalVisible(true)}>
-                <Text style={{color: '#D4D4D4', fontSize: 16}}>
-                  Tìm kiếm mộ liệt sĩ
-                </Text>
-                {filterIcon(vw(5), vw(5), 'white')}
-              </TouchableOpacity>
-              {searchIcon(vw(6), vw(6), 'white')}
+              {searchIcon(vw(6), vw(6), '#547958')}
             </View>
           </View>
-        ) : (
-          <View style={styles.searchIconContainer}>
-            <TouchableOpacity
-              onPress={() => {
-                setIsTextVisible(true);
-              }}>
-              <View style={styles.searchCenterBtn}>
-                {searchIcon(vw(7), vw(7), 'white')}
-              </View>
-            </TouchableOpacity>
+          <View
+            style={{
+              marginTop: vh(1),
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Text style={{color: 'white', fontSize: 12}}>Bộ lọc: </Text>
+            {renderFilterValues()}
           </View>
-        )}
-      </View>
+        </View>
+      )}
     </View>
   );
 };
