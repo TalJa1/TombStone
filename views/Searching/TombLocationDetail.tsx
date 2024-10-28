4dc5042fc8e357bc5a41aff41835ed8dbd273d52
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-28 10:31:54 +0700

    Refactor svgXML to add color parameter to searchIcon function

--- views/Searching/TombLocationDetail.tsx
+++ views/Searching/TombLocationDetail.tsx
@@ -7,12 +7,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import useStatusBar from '../../services/useStatusBar';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {InforDetail} from '../../services/typeProps';
 import {RouteProp, useRoute} from '@react-navigation/native';
-import {backIcon} from '../../assets/svgXML';
+import {backIcon, searchIcon} from '../../assets/svgXML';
 import {vh, vw} from '../../services/styleSheet';
 
 const TombLocationDetail = () => {
@@ -32,6 +32,12 @@ const TombLocationDetail = () => {
 };
 
 const Header: React.FC<{title: string}> = ({title}) => {
+  const [isTextVisible, setIsTextVisible] = useState(true);
+
+  const handleHidePress = () => {
+    setIsTextVisible(false);
+  };
+
   return (
     <View>
       <Image
@@ -46,15 +52,32 @@ const Header: React.FC<{title: string}> = ({title}) => {
           <Text style={styles.headerTitle}>NTLS {title}</Text>
         </View>
       </View>
-      <View style={styles.headerDes}>
-        <Text style={styles.headerDesTxt}>
-          Nghĩa trang liệt sĩ Quốc gia {title} là nơi quy tụ 10.263 phần mộ của
-          các liệt sĩ có tổng diện tích 140.000m2. Phần đất mộ được phân thành
-          10 khu vực chính. Nghĩa trang liệt sĩ Trường Sơn là nơi an nghỉ đời
-          đời của các anh hùng liệt sĩ đã hi sinh trên tuyến đường mòn Hồ Chí
-          Minh trong thời kì chống Mỹ cứu nước.{' '}
-          <Text style={styles.hideButton}>Ẩn</Text>
-        </Text>
+      <View style={{width: vw(100)}}>
+        {isTextVisible ? (
+          <View style={styles.headerDes}>
+            <Text style={styles.headerDesTxt}>
+              Nghĩa trang liệt sĩ Quốc gia {title} là nơi quy tụ 10.263 phần mộ
+              của các liệt sĩ có tổng diện tích 140.000m2. Phần đất mộ được phân
+              thành 10 khu vực chính. Nghĩa trang liệt sĩ Trường Sơn là nơi an
+              nghỉ đời đời của các anh hùng liệt sĩ đã hi sinh trên tuyến đường
+              mòn Hồ Chí Minh trong thời kì chống Mỹ cứu nước.{' '}
+              <Text style={styles.hideButton} onPress={handleHidePress}>
+                Ẩn
+              </Text>
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.searchIconContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                setIsTextVisible(true);
+              }}>
+              <View style={styles.searchCenterBtn}>
+                {searchIcon(vw(7), vw(7), 'white')}
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -100,5 +123,18 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     fontStyle: 'italic',
     color: '#ECF3A3',
+  },
+  searchIconContainer: {
+    alignItems: 'center',
+    backgroundColor: '#547958',
+    height: vh(3),
+    width: vw(100),
+  },
+  searchCenterBtn: {
+    padding: vw(1),
+    borderRadius: vw(50),
+    backgroundColor: '#547958',
+    position: 'relative',
+    top: vh(-2.5),
   },
 });
