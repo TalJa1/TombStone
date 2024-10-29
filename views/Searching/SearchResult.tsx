cd100cb6dd04b02c41d919f9d94c5f1aeca4eab9
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-29 15:35:59 +0700

    Refactor SearchResult component to add header with back button

--- views/Searching/SearchResult.tsx
+++ views/Searching/SearchResult.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {InforDetail, MartyrSearchViewDataProps} from '../../services/typeProps';
 import {martyrSearchData} from '../../services/renderData';
+import {backIcon} from '../../assets/svgXML';
+import {vw} from '../../services/styleSheet';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const SearchResult = () => {
   useStatusBar('white');
   const route = useRoute<RouteProp<InforDetail, 'SearchData'>>();
   const filterData: Partial<Record<keyof MartyrSearchViewDataProps, string>> =
     route.params.data;
-  const [renderData, setRenderData] =
-    useState<MartyrSearchViewDataProps[]>();
-
-  console.log('renderData', renderData);
+  const [renderData, setRenderData] = useState<MartyrSearchViewDataProps[]>();
 
   useEffect(() => {
     const filteredData = martyrSearchData.filter(item => {
@@ -36,10 +42,24 @@ const SearchResult = () => {
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={{flex: 1}}>
-          <Text>SearchResult</Text>
+          <Header />
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity
+        style={styles.backButtton}
+        onPress={() => navigation.goBack()}>
+        {backIcon(vw(6), vw(6))}
+      </TouchableOpacity>
+      <Text style={styles.headerLabel}>Tìm kiếm hồ sơ liệt sĩ</Text>
+    </View>
   );
 };
 
@@ -47,4 +67,17 @@ export default SearchResult;
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: 'white'},
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: vw(3),
+    paddingHorizontal: vw(5),
+    columnGap: vw(3),
+  },
+  backButtton: {
+    backgroundColor: '#91A89526',
+    padding: vw(1.5),
+    borderRadius: vw(50),
+  },
+  headerLabel: {fontSize: 18, fontWeight: '700', color: '#547958'},
 });
