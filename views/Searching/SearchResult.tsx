d43a5dc2257920e145df1861f3a207e6fda514d4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-29 15:46:55 +0700

    Refactor SearchResult component to add filter functionality and adjust styling

--- views/Searching/SearchResult.tsx
+++ views/Searching/SearchResult.tsx
@@ -12,7 +12,7 @@ import useStatusBar from '../../services/useStatusBar';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {InforDetail, MartyrSearchViewDataProps} from '../../services/typeProps';
 import {martyrSearchData} from '../../services/renderData';
-import {backIcon} from '../../assets/svgXML';
+import {backIcon, filterIcon, searchIcon} from '../../assets/svgXML';
 import {vh, vw} from '../../services/styleSheet';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
@@ -38,26 +38,122 @@ const SearchResult = () => {
     setRenderData(filteredData);
   }, [filterData]);
 
+  const renderFilterValues = () => {
+    return Object.keys(filterData)
+      .filter(
+        key => filterData[key as keyof MartyrSearchViewDataProps] !== null,
+      )
+      .map(key => {
+        const value = filterData[key as keyof MartyrSearchViewDataProps];
+        let style;
+
+        if (value === 'Chưa xác định') {
+          style = {
+            backgroundColor: '#F7DAD4',
+            color: '#571B0F',
+          };
+        } else {
+          const randomStyle = Math.floor(Math.random() * 3);
+          switch (randomStyle) {
+            case 0:
+              style = {
+                color: 'white',
+                borderColor: 'white',
+                borderWidth: 1,
+              };
+              break;
+            case 1:
+              style = {
+                backgroundColor: '#ECF3A3',
+                color: '#547958',
+              };
+              break;
+            case 2:
+              style = {
+                backgroundColor: '#F7DAD4',
+                color: '#571B0F',
+              };
+              break;
+          }
+        }
+
+        return (
+          <View key={key}>
+            {value !== '' && (
+              <Text
+                style={[
+                  {fontSize: 12, padding: 5, borderRadius: 5, margin: 2},
+                  style,
+                ]}>
+                {value}
+              </Text>
+            )}
+          </View>
+        );
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={{flex: 1}}>
           <Header />
-          <Main data={renderData} />
+          <Main data={renderData} filterLayout={renderFilterValues} />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const Main: React.FC<{data: MartyrSearchViewDataProps[] | undefined}> = ({
-  data,
-}) => {
+const Main: React.FC<{
+  data: MartyrSearchViewDataProps[] | undefined;
+  filterLayout: () => React.JSX.Element[];
+}> = ({data, filterLayout}) => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <>
+      <View style={styles.headerDes}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            columnGap: vw(4),
+            marginTop: vh(1.5),
+          }}>
+          <TouchableOpacity
+            style={[styles.searchContainer, {borderRadius: 8}]}
+            onPress={() => navigation.goBack()}>
+            <Text style={{color: '#D4D4D4', fontSize: 16}}>
+              Tìm kiếm mộ liệt sĩ
+            </Text>
+            {filterIcon(vw(5), vw(5), 'white')}
+          </TouchableOpacity>
+          <View
+            style={{
+              backgroundColor: 'white',
+              padding: vw(2),
+              borderRadius: 16,
+            }}>
+            {searchIcon(vw(6), vw(6), '#547958')}
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: vh(1),
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: 'white', fontSize: 12}}>Bộ lọc: </Text>
+          {filterLayout()}
+        </View>
+      </View>
       {data && (
         <View style={{paddingHorizontal: vw(5)}}>
-          <Text style={styles.mainTxt}>{`Tổng: ${data.length} kết quả phù hợp`}</Text>
+          <Text
+            style={
+              styles.mainTxt
+            }>{`Tổng: ${data.length} kết quả phù hợp`}</Text>
           <View style={{rowGap: vh(1)}}>
             {data.map((item, index) => {
               return (
@@ -158,5 +254,21 @@ const styles = StyleSheet.create({
   mainTxt: {
     color: '#868686',
     marginBottom: vh(1),
+  },
+  headerDes: {
+    paddingHorizontal: vw(5),
+    paddingVertical: vh(1),
+    backgroundColor: '#547958',
+  },
+  searchContainer: {
+    borderWidth: 1,
+    borderColor: 'white',
+    alignSelf: 'flex-start',
+    flex: 1,
+    padding: vw(2),
+    borderRadius: vw(50),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
